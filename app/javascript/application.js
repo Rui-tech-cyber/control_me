9c409176284d0af8478071c3b483b591bc40cb24
Rui-tech-cyber <rui61louis61@gmail.com>
2026-01-31 13:12:06 +0900

    create environment

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import "./components/Clock";
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Clock from "./components/Clock";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("react-root");
+    if (root) {
+        createRoot(Root).render(<Clock />);
+    }
+});
---
+++ app/javascript/components/Clock.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Clock() {
+    return <h2>ControlMe 起動中...</h2>;
+}
