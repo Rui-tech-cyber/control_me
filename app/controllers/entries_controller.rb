bf0f9cfeef7ab1b9b521229109c1ebdf53ebe219
Rui-tech-cyber <rui61louis61@gmail.com>
2026-02-03 00:46:40 +0900

    fix some wrong model name

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -25,16 +25,16 @@ class EntriesController < ApplicationController
 
     @entries = case @period
                 when "week"
-                  base_scope.where(recorded_on: 6.days.ago.to_date..Date.today)
+                  base_scope.where(recorded_at: 6.days.ago.beginning_of_day..Time.current)
                 when "month"
-                  base_scope.where(recorded_on: Date.today.beginning_of_month..Date.today)
+                  base_scope.where(recorded_at: Time.current.beginning_of_month..Time.current)
                 when "all"
                   base_scope
                 else
                   base_scope
                 end
 
-      @entries = @entries.order(recorded_on: :desc, created_at: :desc)
+      @entries = @entries.order(recorded_at: :desc, created_at: :desc)
 
       prepare_entries_for_view
       prepare_graph_data
@@ -55,7 +55,7 @@ class EntriesController < ApplicationController
         @entries
       end
 
-    @entries_by_date = entries_for_list.group_by(:recorded_on)
+    @entries_by_date = entries_for_list.group_by{ |e| e.recorded_at.to_date }
   end
 
   def prepare_graph_data
@@ -63,16 +63,16 @@ class EntriesController < ApplicationController
 
     @graph_data = case @period
                   when "week", "month"
-                    @entries.group(:recorded_on).sum(:value).sort
+                    @entries.group(:recorded_at).sum(:value).sort
                   when "all"
-                    @entries.group("DATE_TRUNC('month', recorded_on)").sum(:value).sort
+                    @entries.group("DATE_TRUNC('month', recorded_at)").sum(:value).sort
                   else
                     {}
                   end
   end
 
   def prepare_recent_memos
-    @recent_memos = Memo.joins(:record).where(records: { category: @category }).where.not(body: [nil, ""]).order(update_at: :desc).limit(5)
+    @recent_memos = Memo.joins(:entry).where(entries: { category: @category }).where.not(body: [nil, ""]).order(update_at: :desc).limit(5)
   end
 
 end
